3c45f19af21299b8692b2ea1900076d0131b3d7d
kent.andersen <kent.andersen@moneydesktop.com>
2012-11-19 20:23:03 -0700

    start of tablet navigation. Lots of refinement still needed.

--- MoneyMobile/src/com/moneydesktop/finance/tablet/activity/DashboardTabletActivity.java
+++ MoneyMobile/src/com/moneydesktop/finance/tablet/activity/DashboardTabletActivity.java
@@ -5,6 +5,7 @@ import com.moneydesktop.finance.R;
 import com.moneydesktop.finance.data.SyncEngine;
 import com.moneydesktop.finance.model.EventMessage.SyncEvent;
 import com.moneydesktop.finance.util.DialogUtils;
+import com.moneydesktop.finance.views.CircleNavView;
 
 import de.greenrobot.event.EventBus;
 
@@ -12,10 +13,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class DashboardTabletActivity extends BaseActivity {
 
-	Button mTempButton; 
+	Button mTempButton, mLaunchNav;
+	LinearLayout mContainer;
+	CircleNavView mCircleNav;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,16 @@ public class DashboardTabletActivity extends BaseActivity {
         setContentView(R.layout.dashboard_view);
         
         mTempButton = (Button) findViewById(R.id.view_accounts_button);
+        mLaunchNav = (Button) findViewById(R.id.view_nav_button);
+        mContainer = (LinearLayout) findViewById(R.id.dashboard_container);
+        mCircleNav = (CircleNavView) findViewById(R.id.tablet_nav);
+        
+        mLaunchNav.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {  
+                mCircleNav.setVisibility(View.VISIBLE);
+			}
+		});
         
         mTempButton.setOnClickListener(new View.OnClickListener() {
 			
