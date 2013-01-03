bcd07e83b226a46fea2318ff15784fb8089a19db
Matthew <matthew.pulsipher@moneydesktop.com>
2013-01-03 15:19:23 -0700

    I have the layout drafted, isn't behaving properly for some reason.

--- MoneyMobile/src/com/moneydesktop/finance/handset/fragment/TransactionSummaryHandsetFragment.java
+++ MoneyMobile/src/com/moneydesktop/finance/handset/fragment/TransactionSummaryHandsetFragment.java
@@ -6,9 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.moneydesktop.finance.BaseFragment;
 import com.moneydesktop.finance.R;
+import com.moneydesktop.finance.util.Fonts;
 
 public class TransactionSummaryHandsetFragment extends BaseFragment {
 
@@ -37,12 +39,58 @@ public class TransactionSummaryHandsetFragment extends BaseFragment {
         super.onCreateView(inflater, container, savedInstanceState);
 
         mRoot = inflater.inflate(R.layout.handset_transaction_summary_view, null);
-        setupView();
+        setupView(mRoot);
 
         return mRoot;
     }
 
-    private void setupView() {
+    // TODO: fill with actual data, stubbing for now.
+    private void setupView(View v) {
+        TextView mainLabel = (TextView) v.findViewById(R.id.label_transactions_view);
+        Fonts.applySecondaryItalicFont(mainLabel, 16);
+
+        TextView incomeBalanceIcon = (TextView) v.findViewById(R.id.income_balance_icon);
+        Fonts.applyGlyphFont(incomeBalanceIcon, 24);
+        TextView incomeBalance = (TextView) v.findViewById(R.id.income_balance);
+        Fonts.applyPrimaryBoldFont(incomeBalance, 24);
+        TextView incomeBalanceLabel = (TextView) v.findViewById(R.id.income_balance_label);
+        Fonts.applySecondaryItalicFont(incomeBalanceLabel, 16);
+
+        TextView expenseBalanceIcon = (TextView) v.findViewById(R.id.expense_balance_icon);
+        Fonts.applyGlyphFont(expenseBalanceIcon, 24);
+        TextView expenseBalance = (TextView) v.findViewById(R.id.expense_balance);
+        Fonts.applyPrimaryBoldFont(expenseBalance, 24);
+        TextView expenseBalanceLabel = (TextView) v.findViewById(R.id.expense_balance_label);
+        Fonts.applySecondaryItalicFont(expenseBalanceLabel, 16);
+
+        TextView dailySpendingText = (TextView) v.findViewById(R.id.daily_spending_text);
+        Fonts.applySecondaryItalicFont(dailySpendingText, 16);
+
+        TextView newTransactionsNumber = (TextView) v.findViewById(R.id.new_transactions_number);
+        Fonts.applyPrimaryBoldFont(newTransactionsNumber, 24);
+        TextView newTransactionsLabel = (TextView) v.findViewById(R.id.new_transactions_label);
+        Fonts.applySecondaryItalicFont(newTransactionsLabel, 16);
+
+        TextView topCategoryIcon = (TextView) v.findViewById(R.id.top_category_icon);
+        Fonts.applyPrimaryBoldFont(topCategoryIcon, 24);
+        TextView topCategoryText = (TextView) v.findViewById(R.id.top_category_text);
+        Fonts.applySecondaryItalicFont(topCategoryText, 16);
+
+        TextView uncategorizedTransactionIcon = (TextView) v
+                .findViewById(R.id.uncategorized_transaction_icon);
+        Fonts.applyPrimaryBoldFont(uncategorizedTransactionIcon, 24);
+
+        TextView uncategorizedTransactionText = (TextView) v
+                .findViewById(R.id.uncategorized_transaction_text);
+        Fonts.applyPrimaryFont(uncategorizedTransactionText, 16);
+
+        // Stub Data
+        incomeBalance.setText("$0.00");
+        expenseBalance.setText("$0.00");
+        newTransactionsNumber.setText("0");
+        topCategoryIcon.setText("?");
+        uncategorizedTransactionText.setText("You have no uncategorized transactions.");
+
     }
 
     @Override
