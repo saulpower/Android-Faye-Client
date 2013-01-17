d059c53ef3e28bfebbfe01fdde04d5a833b5633b
kent.andersen <kent.andersen@moneydesktop.com>
2013-01-17 09:28:54 -0700

    adding missing folder for sync service

---
+++ MoneyMobile/src/com/moneydesktop/finance/shared/Services/SyncService.java
@@ -0,0 +1,41 @@
+package com.moneydesktop.finance.shared.Services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.moneydesktop.finance.data.SyncEngine;
+
+
+//This method automatically runs/creates on a separate thread. Unlike extending "Service" 
+public class SyncService extends IntentService{
+    
+    public SyncService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        
+        SyncEngine.sharedInstance().beginSync();            
+        
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+        return super.onStartCommand(intent,flags,startId);
+    }
+
+}
